8a92194d584a6deb19ad42dd63172cac829cbcd6
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-02-05 13:44:46 +0200

    Function a*b

---
+++ _GitHub_/_GitHub_/Daniela.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _GitHub_
+{
+    internal class Daniela
+    {
+        static double Number(double a, double b)
+        {
+            return a * b;
+        }
+        static  void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Number(a, b));
+        }
+    }
+}
