1fbe0bbddaf078fa70e64b06584f20c5483bba6c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-02-05 13:48:36 +0200

    Function a*b

---
+++ _GitHub_/_GitHub_/Dani.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _GitHub_
+{
+    internal class Dani
+    {
+        static int Lmao(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
