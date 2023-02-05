a04be831d383b75370945a1c82e43c9afc28b44f
Mitian lmao <turgoww@gmail.com>
2023-02-05 14:19:01 +0200

    ha

--- _GitHub_/_GitHub_/Program.cs
+++ _GitHub_/_GitHub_/Program.cs
@@ -3,9 +3,14 @@ static int Lmao(int a, int b)
     {
     return a + b;
     }
+static double Smeshen(double a, double b)
+{
+    return a * b;
+}
 static void Main()
 {
     int c = int.Parse(Console.ReadLine());
     int d = int.Parse(Console.ReadLine());
     Console.WriteLine(Lmao(c, d));
+    Console.WriteLine(Smeshen(c, d));
 }
